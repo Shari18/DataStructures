eead82397543a6afd4bcb968f2920b85922ca27f
Sharathkumar Hegde <Sharathkumar.harishi@gmail.com>
2022-09-24 13:48:25 +0530

    Updated min heap with private methods

--- MinHeap.py
+++ MinHeap.py
@@ -2,7 +2,7 @@ class MinHeap:
     def __init__(self):
         self.heap = []
 
-    def get_size(self):
+    def __len__(self):
         return len(self.heap)
 
     def get_min(self):
@@ -10,17 +10,17 @@ class MinHeap:
             return self.heap[0]
         raise Exception("Heap is empty")
 
-    def parent(self, index):
+    def _parent(self, index):
         return (index - 1) // 2
 
-    def left_child(self, index):
+    def _left_child(self, index):
         return 2 * index + 1
 
-    def right_child(self, index):
+    def _right_child(self, index):
         return 2 * index + 2
 
-    def rise(self, index):
-        parent_index = self.parent(index)
+    def _rise(self, index):
+        parent_index = self._parent(index)
         while index >= 0 and parent_index >= 0 and self.heap[parent_index] > self.heap[index]:
             # swap
             self.heap[parent_index], self.heap[index] = (
@@ -28,21 +28,22 @@ class MinHeap:
                 self.heap[parent_index],
             )
             index = parent_index
-            parent_index = self.parent(index)
+            parent_index = self._parent(index)
 
     def push(self, x):
         self.heap.append(x)
-        self.rise(len(self.heap) - 1)
+        self._rise(len(self.heap) - 1)
 
-    def sink(self, index):
+    def _sink(self, index):
         while True:
-            child_index = self.left_child(index)
-            if child_index >= len(self.heap):
+            left_child_index = self._left_child(index)
+            if left_child_index >= len(self.heap):
                 break
-            right_child_index = self.right_child(index)
+            child_index = left_child_index
+            right_child_index = self._right_child(index)
             if (
                 right_child_index < len(self.heap)
-                and self.heap[right_child_index] < self.heap[child_index]
+                and self.heap[right_child_index] < self.heap[left_child_index]
             ):
                 child_index = right_child_index
             if self.heap[index] > self.heap[child_index]:
@@ -61,15 +62,15 @@ class MinHeap:
         :return:
         """
         # swap last and first item
-        self.heap[0], self.heap[len(self.heap) - 1] = (
-            self.heap[len(self.heap) - 1],
+        self.heap[0], self.heap[-1] = (
+            self.heap[-1],
             self.heap[0],
         )
         x = self.heap.pop()
-        self.sink(0)
+        self._sink(0)
         return x
 
     def build_heap_from_random_arr(self, arr):
         self.heap = arr
         for i in range((len(arr) // 2) - 1, -1, -1):
-            self.sink(i)
+            self._sink(i)
